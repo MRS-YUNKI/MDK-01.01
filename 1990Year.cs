b416a7e7d91a46ad481ece5f7b49b9b61296d1c9
MRS-YUNKI <150360411+MRS-YUNKI@users.noreply.github.com>
2024-10-18 09:52:07 +0300

    Update 1990Year.cs
    Changes in code (+day of week)

--- 1990Year.cs
+++ 1990Year.cs
@@ -1,15 +1,14 @@
-﻿using System.Globalization;
-
 Console.WriteLine("Введите количество месяцев: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
 DateTime StartDate = new DateTime(1990, 1, 1);
 DateTime ResultDate = StartDate.AddMonths(N).AddDays(2);
 
-string NameOfMonth = ResultDate.ToString("MMMM", CultureInfo.InvariantCulture);
+string NameOfMonth = ResultDate.ToString("MMMM");
+string NameOfDay = ResultDate.ToString("dddd");
 int Day = ResultDate.Day;
 int Year = ResultDate.Year;
 bool LeapYear = DateTime.IsLeapYear(Year);
 
-Console.WriteLine($"День, месяц и год полученного года: {Day} {NameOfMonth} {Year}");
+Console.WriteLine($"День, месяц, год, день недели полученного года: {Day} {NameOfMonth} {Year}, {NameOfDay}");
 Console.WriteLine($"Полученный год является високосным или нет: {(LeapYear ? "Да" : "Нет")}");
