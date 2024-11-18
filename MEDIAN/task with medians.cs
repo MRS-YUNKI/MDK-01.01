ec33d33b75f7d6378f8c955fc9b13a107f013c15
MRS-YUNKI <150360411+MRS-YUNKI@users.noreply.github.com>
2024-11-18 09:15:53 +0300

    Task with medians

---
+++ MEDIAN/task with medians.cs
@@ -0,0 +1,48 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        MedianFinder medianFinder = new MedianFinder();
+        Console.WriteLine("Введите числа для нахождения медианы (после завершения ввода чисел введите 'exit' для выхода из ввода):");
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input == "exit")
+            {
+                break;
+            }
+            if (int.TryParse(input, out int num))
+            {
+                medianFinder.AddNum(num);
+            }
+        }
+        Console.WriteLine($"Медиана: {medianFinder.FindMedian()}");
+    }
+}
+
+public class MedianFinder
+{
+    private List<int> numbers;
+    public MedianFinder()
+    {
+        numbers = new List<int>();
+    }
+
+    public void AddNum(int num)
+    {
+        numbers.Add(num);
+    }
+
+    public double FindMedian()
+    {
+        int count = numbers.Count;
+        if (count % 2 != 0)
+        {
+            return numbers[count / 2];
+        } else
+        {
+            return (numbers[count / 2] + numbers[count / 2 - 1]) / 2.0;
+        }
+    }
+}
