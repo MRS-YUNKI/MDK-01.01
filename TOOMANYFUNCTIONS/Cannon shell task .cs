99f7a6ef227d4fa23bdd88ffb52f4ec15c4bed8f
MRS-YUNKI <150360411+MRS-YUNKI@users.noreply.github.com>
2024-12-24 14:16:38 +0300

    updated.

--- TOOMANYFUNCTIONS/Cannon shell task .cs
+++ TOOMANYFUNCTIONS/Cannon shell task .cs
@@ -18,16 +18,21 @@ double h_max = v0 * Math.Sin(angle_in_radian) * t_max - ((g * Math.Pow(t_max, 2)
 double t = 0;
 double dt = 0.1;
 
+StreamWriter file = new StreamWriter("C:\\Users\\prdb\\Desktop\\Demo\\Sch\\MDKMDK\\TRAINING\\OUTPUT.txt");
+file.WriteLine("Таблица значений:");
+file.WriteLine("t\t\t\t\t\tx\t\t\t\t\ty");
+file.WriteLine("________________________________________________________________________________________________________________________");
 while (true)
 {
     double x = x0 + vx0 * t;
-    double y = y0 + vy0 * t - (g * Math.Pow(t, 2)) / 2;
+    double y = y0 + vy0 * t - ((g * Math.Pow(t, 2)) / 2);
+
     if (y <= 0)
     {
-        Console.WriteLine($"Снаряд упал в координатах: ({x}, 0)");
-        Console.WriteLine($"Максимальная высота полета снаряда равняется {h_max}.");
+        file.WriteLine($"Снаряд упал в координатах ({x}, 0).");
         break;
     }
-    Console.WriteLine($"t = {t}, x = {x}, y = {y}.");
+    file.WriteLine($"t = {t}\t\t\t\t\tx = {x}\t\t\t\t\ty = {y}");
     t += dt;
 }
+file.Close();
